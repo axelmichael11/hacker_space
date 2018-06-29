b908f70dfed7c7724fd6552e373bb69ebeda0a47
Michael <axelmichael11@gmail.com>
2018-06-29 10:28:32 -0700

    mvp is achieved, moving back and front end projects to separate repos

--- poller-frontend/src/component/my-polls/no-polls.js
+++ poller-frontend/src/component/my-polls/no-polls.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Paper from 'material-ui/Paper'
+import {compose, branch, renderComponent} from 'recompose'
 
 import {VictoryPie, VictoryLegend, VictoryTooltip, VictoryLabel} from 'victory'
 import AppBar from 'material-ui/AppBar'
@@ -16,7 +17,7 @@ import Typography from '@material-ui/core/Typography';
 class NoPolls extends React.Component {
   render(){
       return(
-        <div className="no-data">
+        <div>
             <Typography variant="headline" component="h3" style={{width:'100%' , margin:'auto', textAlign:'center' }}>You don't have any polls...</Typography>
         </div>
       )
